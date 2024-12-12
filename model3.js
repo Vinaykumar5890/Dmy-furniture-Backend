af05bf5ad18f70dfdafc6600e80c94066d420fcd
vinaykumar <156113745+Vinaykumar5890@users.noreply.github.com>
2024-12-12 16:21:43 +0530

    Create model3.js

---
+++ model3.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+let Post = new mongoose.Schema({
+  image: { 
+    type: String, 
+      required: true }, 
+  
+  description: { type: String,
+                required: true },
+  
+  likes: { type: Number, 
+          default: 0 },
+  
+  createdAt: { type: Date, 
+              default: Date.now }
+})
+
+module.exports = mongoose.model('Post', Post)
