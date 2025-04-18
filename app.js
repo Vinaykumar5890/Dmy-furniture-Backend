c55d610c86cc7aee65539b3e4f19765066b09c45
vinaykumar <156113745+Vinaykumar5890@users.noreply.github.com>
2025-04-18 11:19:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ require('dotenv').config();
 
 app.use(express.json())
 app.use(cors({origin: '*'}))
-
+const man = async ()=>{
 await mongoose
   .connect(process.env.MONGO_URI,{
       useNewUrlParser: true,
@@ -23,6 +23,7 @@ await mongoose
   )
   .then(() => console.log('DB Connected'))
   .catch(err => console.log(err))
+}
 }
 function authenticateToken(request, response, next) {
   let jwtToken
